62cd7a10d1a312281e4a69d9907c74c555e853e9
alexanduh <alexander.j.peng@gmail.com>
2020-09-30 18:27:41 -0700

    finished calculate age challenge

---
+++ functions.py
@@ -0,0 +1,47 @@
+# def greet_customer(special):
+#     return "Our special today is " + special
+
+# print(greet_customer("banana yogurt"))
+
+# def add_one_if_odd(num):
+#     # print(num+1)
+#     if num % 2 != 0:
+#         return num + 1, 
+#     else:
+#         return num
+
+# def add_two(num):
+#     return num + 2
+
+# x = add_two(add_one_if_odd(3))
+# y = add_two(2)
+
+# # num == 1
+
+# print(x)
+
+
+
+y = 1
+
+def square_point_plus_ten(x, y):
+    z = 10
+    print(z)
+    x_2 = x*x + z
+    y_2 = y*y + z
+    return x_2, y_2
+    
+
+# x_squared, y_squared = square_point_plus_ten(2, 4)
+
+# print(str(x_squared) + " " + str(y_squared))
+
+square_point_plus_ten(1,2)
+
+print(z)
+
+
+
+
+
+
---
+++ math_and_operators.py
@@ -0,0 +1,63 @@
+# x = 5
+# y = 10
+# z = 2
+
+# print(x)
+# print(y)
+# print(x+y)
+
+# print(x*y)
+# print(x-y)
+# print((x+y)*(y+z))
+
+# x = 7
+# print(x)
+# print(y)
+
+# print(2**8)
+# 256
+
+# print(7/5)
+# 1.4
+
+# print(7%5)
+# 2
+
+# num_given = int(input("Give a number"))
+
+# if(num_given % 2 == 0):
+#     print("even")
+# else:
+#     print("odd")
+
+# num = 5
+# print(num%2)
+
+
+# str1 = "hello"
+# num2 = 5
+
+# print(str1 + " " + str(num2))
+
+# x = "5"
+
+# x += "2"
+
+# print(x)
+
+# x = 5
+
+# x += 2
+
+# print(x)
+
+# multi_string = """
+# Hello
+# I
+# Am
+# A
+# Multi-line
+# String
+# """
+
+# print(multi_string)
